a002343fd356a5f5818b08ddfc3bd21baf016ba8
P. Mark Anderson <mark@spotmetrix.com>
2013-12-02 19:14:28 -0800

    crawlers

--- LEDStripParticleEmitter.cpp
+++ LEDStripParticleEmitter.cpp
@@ -9,60 +9,81 @@ MIT license
 
 /*****************************************************************************/
 
-ParticleEmitter::ParticleEmitter(uint16_t n) {
-    numPixels = n;
+ParticleEmitter::ParticleEmitter(uint16_t _numPixels, uint8_t _maxColor) {
+    maxColor = fmin(MAX_COLOR, fmax(0, _maxColor));
+    numPixels = _numPixels;
     numParticles = MAX_PARTICLES;
     maxVelocity = (random(100) / 100.0 * 0.028) + 0.004; //float(numParticles / 5000.0);
     stripPosition = 0.5;
+    masterVelocityDeltaDirection = 1;
     
-    for (int i=0; i < MAX_PARTICLES; i++) {
+    for (int i=0; i < numParticles; i++) {
         particles[i] = newParticle();
     }
 }
 
-particle ParticleEmitter::newParticle() {
-    particle p;
+Particle ParticleEmitter::newParticle() {
+    Particle p;
     
-    int8_t direction = (random(2) == 0 ? 1 : -1);
-    uint8_t maxColor = MAX_COLOR * (1.0 - (random(80) / 100));
-    // maxColor = (random(10) > 4) ? maxColor : 0.0;
-    // uint8_t maxColor = 0;
+    p.coord.x = random(20) / 100.0 + 0.4;
+    p.coord.y = random(20) / 100.0 + 0.4;
+    p.coord.z = 0;
 
-    p.velocity = ((random(89) + 10) / 100.0) * direction;    
-    p.redColor = random(maxColor);
-    p.greenColor = random(maxColor);
-    p.blueColor = random(maxColor);    
-    p.startTime = millis();
-    p.startStripPosition = stripPosition;
-    p.currentStripPosition = p.startStripPosition;
-    p.dimmed = 0;
+    int8_t direction = p.coord.x > 0.5 ? 1 : -1;
+    // int8_t direction = (random(2) == 0 ? 1 : -1);    
+    // p.velocity.x = ((random(89) + 10) / 100.0) * direction;
+    p.velocity.x = ((random(89) + 10) / 100.0) * direction;
+    direction = (random(2) == 0 ? 1 : -1);    
+    p.velocity.y = ((random(89) + 10) / 100.0) * direction;
 
+    uint8_t prtMaxColor = maxColor * (1.0 - (random(50) / 100.0));
+    // prtMaxColor = (random(10) > 4) ? prtMaxColor : 0.0;
+    // uint8_t prtMaxColor = 0;
+    p.redColor = random(prtMaxColor);
+    p.greenColor = random(prtMaxColor);
+    p.blueColor = random(prtMaxColor);    
+    p.dimmed = false;
+
+    masterVelocityDeltaDirection = !masterVelocityDeltaDirection;
     return p;
 }
 
 void ParticleEmitter::begin(void) {
 }
 
-particle ParticleEmitter::updateParticle(uint16_t i, boolean respawn) {
-    particle *p = &particles[i];
-    
-    p->currentStripPosition = p->currentStripPosition +
-                              (maxVelocity * p->velocity);  // *
-                              //float(millis() - p->startTime);
-
+Particle ParticleEmitter::updateParticle(uint16_t i, boolean respawn) {
+    Particle *p = &particles[i];    
+    p->coord.x = p->coord.x + (maxVelocity * p->velocity.x);
+    p->coord.y = p->coord.y + (maxVelocity * p->velocity.y);
     p->dimmed = (random(3) == 0 ? 1 : 0);
+
+    p->velocity.x += maxVelocity  * masterVelocityDeltaDirection; // * (random(10.0) / 10.0 + 0.1);
+    p->velocity.y += maxVelocity  * masterVelocityDeltaDirection; // * (random(10.0) / 10.0 + 0.1);
+
+    if (fabs(p->velocity.x) > 3.0 || 
+        fabs(p->velocity.y) > 3.0) {
+        masterVelocityDeltaDirection *= -1.0;
+    }
 
     if (respawn) {
         if (respawnOnOtherSide) {        
-            if (p->currentStripPosition < 0.0) {
-                p->currentStripPosition = 1.0;            
+            if (p->coord.x < 0.0) {
+                p->coord.x = 1.0;            
             }
-            else if (p->currentStripPosition > 1.0) {
-                p->currentStripPosition = 0.0;            
+            else if (p->coord.x > 1.0) {
+                p->coord.x = 0.0;            
+            }
+
+            if (p->coord.y < 0.0) {
+                p->coord.y = 1.0;            
+            }
+            else if (p->coord.y > 1.0) {
+                p->coord.y = 0.0;            
             }
         }
         else {
-            if (p->currentStripPosition < -1.0 || p->currentStripPosition > 2.0) {
+            if (p->coord.x < 0.0 || p->coord.x > 1.0 ||
+                p->coord.y < 0.0 || p->coord.y > 1.0) {
                 *p = newParticle();            
             }
         }
--- LEDStripParticleEmitter.h
+++ LEDStripParticleEmitter.h
@@ -5,42 +5,49 @@
  #include <pins_arduino.h>
 #endif
 
-#define MAX_PARTICLES 60
+#define MAX_PARTICLES 40 // Watch out for
 
-struct particle {
-    float velocity;
-    uint8_t redColor;
-    uint8_t greenColor;
-    uint8_t blueColor;
-    uint8_t dimmed;
-    unsigned long int startTime;
-    float startStripPosition;
-    float currentStripPosition;
-    bool respawnOnOtherSide;
-};
+typedef struct {
+  float x;
+  float y; 
+  float z;
+} Coord3D;
+
+typedef struct {
+    Coord3D velocity;
+    byte redColor;
+    byte greenColor;
+    byte blueColor;
+    boolean dimmed;
+    Coord3D coord;
+} Particle;
 
 class ParticleEmitter {
 
  public:
 
-  ParticleEmitter(uint16_t n);
+  ParticleEmitter(uint16_t numPixels, uint8_t maxColor);
   ParticleEmitter(void);
   void
     begin(void);
-  particle
+  Particle
     updateParticle(uint16_t i, boolean respawn),
     newParticle();
   float
     stripPosition,
-    maxVelocity;
+    maxVelocity;    
   uint16_t
     numPixels,    // Number of RGB LEDs in strip
     numParticles;
+  uint8_t
+    maxColor;
   bool
     respawnOnOtherSide;
 
  private:
 
-  particle
+  Particle
     particles[MAX_PARTICLES];
+  float
+    masterVelocityDeltaDirection;
 };
